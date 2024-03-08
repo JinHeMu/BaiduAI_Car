2e2b1594ff2fbfcf47de5e66ecb3a6da477a5e34
JinHeMu <490754775@qq.com>
2024-03-08 17:27:34 +0800

    add mapping.cpp(IPM)

---
+++ edgeboard/build/CMakeFiles/icar.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icar.dir/src/icar.cpp.obj"
+  "CMakeFiles/icar.dir/src/icar.cpp.obj.d"
+  "icar.exe"
+  "icar.exe.manifest"
+  "icar.pdb"
+  "libicar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/icar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- edgeboard/src/src/icar.cpp
+++ edgeboard/src/src/icar.cpp
@@ -12,6 +12,7 @@
 //#include "detection/racing.cpp"      //AI检测：追逐区
 //#include "detection/rescue.cpp"      //AI检测：救援区
 //#include "motion.cpp"                //智能车运动控制类
+#include "mapping.cpp"                 //透视变换类
 #include "preprocess.cpp"            //图像预处理类
 #include "recognition/crossroad.cpp" //十字道路识别与路径规划类
 //#include "recognition/ring.cpp"      //环岛道路识别与路径规划类
@@ -32,34 +33,30 @@ int main(int argc, char const *argv[]) {
 //	VideoCapture cap(0);读取设备摄像头
     Mat img;
     Mat imgBin;
+    Mat imageIPM(240, 320, CV_8UC3, Scalar(0, 0, 0));
     Preprocess preprocess;
-    Display display(3); // 创建一个窗口数量为1的对象
+    Mapping IPM(cv::Size(320, 240), cv::Size(COLSIMAGEIPM, ROWSIMAGEIPM));
+    Display display(4); // 创建一个窗口数量为1的对象
 
     while(true)
     {
         cap.read(img);
         Mat imageCorrection = preprocess.correction(img);
         Mat imageBinary = preprocess.binaryzation(imageCorrection);
+        IPM.homography(imageCorrection, imageIPM);
 
         // 设置新窗口的属性并显示图像
         display.setNewWindow(1, "Original Image", img);
         display.setNewWindow(2, "Corrected Image", imageCorrection);
         display.setNewWindow(3, "imageBinary", imageBinary);
+        display.setNewWindow(4, "imageIPM", imageIPM);
 
         // 显示融合后的图像
         display.show();
 
+
         waitKey(1);
     }
-
-
-
-    return 0;
-
-
-
-    return 0;
-
 
 
 
